77ccf3dd4c3d06c7cf90cb21a99464be0f70a34c
Hussein Kandil <hussein.mahmoud.s7s@gmail.com>
2023-09-23 15:08:07 +0300

    Markdown Previewer: Add the core functionalities

--- markdown-previewer/src/App.jsx
+++ markdown-previewer/src/App.jsx
@@ -1,28 +1,50 @@
-import { Component, createRef } from "react";
+import { Component, createRef, Fragment } from "react";
 import { marked } from "marked";
+
+const INITIAL_MARKDOWN = `\
+# Markdown Previewer
+
+## What is Markdown?
+> **Markdown** is a lightweight markup language for creating formatted text using a plain-text editor. \
+John Gruber created Markdown in 2004 as a markup language that is easy to read in its source code form.
+
+Visit [Wikipedia](https://en.wikipedia.org/wiki/Markdown), for more info.
+
+## Inline code in Markdown
+In JavaScript, \`console.log("Hello World!");\` outputs 'Hello World!' on the console screen.
+
+## Code block in Markdown
+\`\`\`
+// In JavaScript, this code outputs 'Hello World' 10 times.
+// Each time on a new line & followed by a number from 1 to 10.
+for (let i = 0; i < 10; i++) {
+  const currentCount = i + 1;
+  console.log("Hello World " + currentCount);
+}
+\`\`\`
+
+## Some of the Markdown variants
+- GitHub Flavored Markdown
+- Markdown Extra
+- LiaScript
+
+#### Best regards, Hussein Kandil
+![A fantasy picture of me setting on the clouds.](https://mir-s3-cdn-cf.behance.net/project_modules/fs/0123ab25920475.5634cd0525408.jpg)
+`;
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.previewDiv = createRef();
     this.state = {
-      pureMarkdown: "# Marked in the browser\n\nRendered by **marked**.",
+      pureMarkdown: INITIAL_MARKDOWN,
     };
     this.handleMarkdownInput = this.processMarkdown.bind(this);
   }
 
-  convertNewLineToBr(text) {
-    let newText = "";
-    for (let i = 0; i < text.length; i++) {
-      newText += text[i] === "\n" ? "<br>" : text[i];
-    }
-    return newText;
-  }
-
   processMarkdown() {
-    // const newLineBecomeBr = this.convertNewLineToBr(this.state.pureMarkdown);
-    const newLineBecomeBr = this.state.pureMarkdown.replace("\r", "").replace("\n", "<br>");
-    console.log(newLineBecomeBr);
+    let newLineBecomeBr = this.state.pureMarkdown; // Temp
+    // newLineBecomeBr = this.state.pureMarkdown.replace(/\n/g, "<br>\n\n");
     this.previewDiv.current.innerHTML = marked.parse(newLineBecomeBr);
   }
 
@@ -36,18 +58,18 @@ class App extends Component {
 
   render() {
     return (
-      <div className="container-fluid">
-        <div className="row">
-          <textarea
-            id="editor"
-            rows={7}
-            placeholder="Enter you markdown here ;)"
-            onChange={(e) => this.setState({ pureMarkdown: e.target.value })}
-            value={this.state.pureMarkdown}
-          ></textarea>
-        </div>
+      <Fragment>
+        <h1 style={{ margin: "2rem auto", textAlign: "center" }}>
+          Markdown Previewer
+        </h1>
+        <textarea
+          id="editor"
+          rows={7}
+          onChange={(e) => this.setState({ pureMarkdown: e.target.value })}
+          value={this.state.pureMarkdown}
+        ></textarea>
         <div id="preview" ref={this.previewDiv}></div>
-      </div>
+      </Fragment>
     );
   }
 }
--- markdown-previewer/src/main.jsx
+++ markdown-previewer/src/main.jsx
@@ -1,8 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App.jsx'
-import "bootstrap/dist/css/bootstrap.min.css"
-import "bootstrap/dist/js/bootstrap.bundle.min.js"
 import './index.css'
 
 // eslint-disable-next-line react/no-deprecated
