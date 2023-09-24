7104786ca6058ac56c2e7c6d7d8c7db2a5045b31
Hussein Kandil <hussein.mahmoud.s7s@gmail.com>
2023-09-24 19:48:06 +0300

    Drum Machine: Initiate the app

--- drum-machine/src/App.jsx
+++ drum-machine/src/App.jsx
@@ -1,24 +1,62 @@
-import { useState } from "react";
-import "./App.css";
+import { Component } from "react";
+import DrumButton from "./component/DrumButton";
 
-function App() {
-  const [count, setCount] = useState(0);
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.ASSETS_PATH = "/src/assets/";
+    this.drums = [
+      { keyName: "Q", drumName: "Heater-1", fileName: "Heater-1.mp3" },
+      { keyName: "W", drumName: "Heater-2", fileName: "Heater-2.mp3" },
+      { keyName: "E", drumName: "Heater-3", fileName: "Heater-3.mp3" },
+      { keyName: "A", drumName: "Heater-4", fileName: "Heater-4.mp3" },
+      { keyName: "S", drumName: "Clap", fileName: "Clap.mp3" },
+      { keyName: "D", drumName: "Open-HH", fileName: "Open-HH.mp3" },
+      { keyName: "Z", drumName: "Kick-n'-Hat", fileName: "Kick-n-Hat.mp3" },
+      { keyName: "X", drumName: "Kick", fileName: "Kick.mp3" },
+      { keyName: "C", drumName: "Closed-HH", fileName: "Closed-HH.mp3" },
+    ];
+  }
 
-  return (
-    <div className="container">
-      <div className="row">
-        <div className="position-absolute top-50 start-50 translate-middle w-100">
-          <h1>Counter App</h1>
-          <button
-            className="btn btn-dark text-danger fs-3 d-block mx-auto my-5 p-3"
-            onClick={() => setCount((count) => count + 1)}
+  componentDidMount() {
+    document.addEventListener("keydown", (event) => {
+      event.preventDefault();
+      switch (event.key) {
+        case "q":
+        case "Q":
+        case "ض":
+          document.getElementById("Q")?.play();
+          break;
+      }
+    });
+  }
+
+  render() {
+    return (
+      <div className="container-fluid">
+        <div
+          className="row m-auto mt-5
+          col-11 col-sm-10 col-md-9 col-lg-7 col-xl-6 col-xxl-5"
+        >
+          <div
+            id="drum-machine"
+            className="d-flex flex-md-row justify-content-between"
           >
-            count is&nbsp;<span className="text-warning">{count}</span>
-          </button>
+            {this.drums.map((drum) => {
+              return (
+                <DrumButton
+                  key={drum.keyName}
+                  keyName={drum.keyName}
+                  filePath={this.ASSETS_PATH + drum.fileName}
+                />
+              );
+            })}
+          </div>
+          <div id="display"></div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
