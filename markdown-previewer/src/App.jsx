f4fa2bd08076eded9e8636bfe436529f88b0ee96
Hussein Kandil <hussein.mahmoud.s7s@gmail.com>
2023-09-24 13:47:58 +0300

    Markdown Previewer: Finish the md parsing functionality with 'marked' library options (including 'breaks' option) and add custom function to parse new lines

--- markdown-previewer/src/App.jsx
+++ markdown-previewer/src/App.jsx
@@ -1,5 +1,6 @@
 import { Component, Fragment } from "react";
-import { marked } from "marked";
+import { Marked } from "marked";
+import { markedHighlight } from "marked-highlight";
 import "highlight.js/styles/github.css";
 import hljs from "highlight.js/lib/common";
 
@@ -9,12 +10,11 @@ const INITIAL_MARKDOWN = `\
 ## What is Markdown?
 > **Markdown** is a lightweight markup language for creating formatted text using a plain-text editor. \
 John Gruber created Markdown in 2004 as a markup language that is easy to read in its source code form.
-Visit [Wikipedia](https://en.wikipedia.org/wiki/Markdown), for more info.
-
+Visit [Wikipedia](https://en.wikipedia.org/wiki/Markdown), for more info.  
+ 
+&nbsp;  
 ## Inline code in Markdown
 In JavaScript, \`console.log("Hello World!"); // Outputs: Hello World!\`
-
-
 
 ## Code block in Markdown
 \`\`\`
@@ -39,7 +39,8 @@ for (let i = 0; i < 10; i++) {
 #### My image in Markdown ;)
 ![A fantasy picture of me setting on the clouds.](https://mir-s3-cdn-cf.behance.net/project_modules/fs/0123ab25920475.5634cd0525408.jpg)
 
-###### Best regards, Hussein Kandil
+###### Best regards, 
+###### Hussein Kandil
 `;
 
 class App extends Component {
@@ -57,47 +58,68 @@ class App extends Component {
     for (let i = 0; i < text.length; i++) {
       if (text[i] === "`" && i !== text.length - 1 && text[i + 1] !== "`") {
         insideCodeElement = !insideCodeElement;
-        newText += text[i];
-        continue;
       }
-      if (!insideCodeElement && text[i] === "\n") {
-        if (i !== 0 && i !== text.length - 1) {
-          if (text[i - 1] === "\n" && text[i + 1] === "\n") {
-            newText += "<br>\n";
-          } else if (text[i - 1] === "\n" && text[i + 1] !== "\n") {
-            newText += "<br>\n\n";
-          } else {
-            newText += text[i];
-          }
-        } else {
-          newText += "<br>\n\n";
-        }
+      if (
+        !insideCodeElement &&
+        text[i] === "\n" &&
+        i !== 0 &&
+        i !== text.length - 1 &&
+        text[i - 1] === "\n" &&
+        text[i + 1] === "\n"
+      ) {
+        newText += "<br>\n";
+      } else if (
+        !insideCodeElement &&
+        text[i] === "\n" &&
+        (i === 0 ||
+          i === text.length - 1 ||
+          (text[i - 1] === "\n" && text[i + 1] !== "\n"))
+      ) {
+        newText += "<br>\n\n";
       } else {
         newText += text[i];
       }
     }
+    //  TODO: any '<br>' following blockquote is caged inside the blockquote!!
+    // You could solve this by filtering the DOM and move line breaks outside blockquote
     return newText;
   }
 
-  highlightAnyCodeSyntax() {
-    Array.from(document.getElementsByTagName("code")).forEach((code) => {
-      code.innerHTML =
-        hljs?.highlightAuto(code.innerHTML).value ?? code.innerHTML;
+  processMarkdown(markdownString) {
+    let newLineBecomeBr = markdownString; // Temp
+    newLineBecomeBr = this.replaceNewLinesWithHTMLBr(markdownString);
+    // Create reference instance configured with marked-highlight
+    const marked = new Marked(
+      markedHighlight({
+        langPrefix: "hljs language-",
+        highlight(code) {
+          // Use auto highlight instead of specific language technique @marked-highlight docs
+          return hljs?.highlightAuto(code).value;
+        },
+      })
+    );
+    // Create renderer to assign a custom '<a>' tag to link rendering
+    const renderer = new marked.Renderer();
+    renderer.link = (href, title, text) =>
+      `<a target="_blank" rel="noreferrer" href="${href}" title="${
+        title ?? text ?? ""
+      }">${text}</a>`;
+    // Set options for 'marked'
+    marked.use({
+      pedantic: false,
+      gfm: true,
+      breaks: true,
+      renderer: renderer,
     });
-  }
-
-  processMarkdown(markdown) {
-    let newLineBecomeBr = markdown; // Temp
-    newLineBecomeBr = this.replaceNewLinesWithHTMLBr(markdown);
-    this.setState({ mdPreview: marked.parse(newLineBecomeBr) });
+    // Compile (parse code with 'marked')
+    const parsed = marked.parse(newLineBecomeBr);
+    this.setState({
+      mdPreview: parsed,
+    });
   }
 
   componentDidMount() {
     this.processMarkdown(INITIAL_MARKDOWN);
-  }
-
-  componentDidUpdate() {
-    this.highlightAnyCodeSyntax();
   }
 
   render() {
